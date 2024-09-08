3659c5dbd457876b4566efc50af859d232034e4a
ajf1016 <83690916+ajf1016@users.noreply.github.com>
2024-09-08 16:22:19 +0530

    rest api setup done

---
+++ sos/api/v1/auth/views.py
@@ -0,0 +1,6 @@
+import requests
+import json
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.models import User
